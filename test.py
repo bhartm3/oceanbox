e174de88a930435f1e8dbf2b7037068291906697
bhart <bhartm3@gmail.com>
2022-12-02 10:01:38 +0100

    add script

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from typing import Tuple
 
 import pygame
-from pygame._sdl2 import get_audio_device_names
+import pygame._sdl2.audio
 pygame.mixer.init()  # Initialize the mixer, this will allow the next command to work
 
 # init_subsystem(INIT_AUDIO)
-names = get_audio_device_names(True)
+names = get_audio_device_names()
 print(names)
 
 pygame.mixer.quit()  # Quit the mixer as it's initialized on your main playback device
