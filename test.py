f3a457f078206e6b298b569d81c3ea8765a2a373
bhart <bhartm3@gmail.com>
2022-12-02 09:32:59 +0100

    add script

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
-import pygame
+from typing import Tuple
 
-pygame.mixer.init() # Initialize the mixer, this will allow the next command to work
-print(pygame._sdl2.audio.get_audio_device_names(False)) # Returns playback devices, Boolean value determines whether they are Input or Output devices.
-pygame.mixer.quit() # Quit the mixer as it's initialized on your main playback device
+import pygame
+import pygame._sdl2.audio as sdl2_audio
+
+pygame.mixer.init()  # Initialize the mixer, this will allow the next command to work
+
+
+def get_devices(capture_devices: bool = False) -> Tuple[str, ...]:
+    init_by_me = not pygame.mixer.get_init()
+    if init_by_me:
+        pygame.mixer.init()
+    devices = tuple(sdl2_audio.get_audio_device_names(capture_devices))
+    if init_by_me:
+        pygame.mixer.quit()
+    return devices
+
+
+print(get_devices())
+
+pygame.mixer.quit()  # Quit the mixer as it's initialized on your main playback device
