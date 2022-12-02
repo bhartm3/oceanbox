a57da10b4ba0dbbd605a4666bb88bcda1aff65f5
bhart <bhartm3@gmail.com>
2022-12-02 09:29:49 +0100

    add script

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import pygame
-import pygame._sdl2 as sdl2
 
 pygame.mixer.init() # Initialize the mixer, this will allow the next command to work
-print(sdl2.audio.get_audio_device_names(False)) # Returns playback devices, Boolean value determines whether they are Input or Output devices.
+print(pygame._sdl2.audio.get_audio_device_names(False)) # Returns playback devices, Boolean value determines whether they are Input or Output devices.
 pygame.mixer.quit() # Quit the mixer as it's initialized on your main playback device
